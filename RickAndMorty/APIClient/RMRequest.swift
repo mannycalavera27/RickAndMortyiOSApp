9e6baac59c32e201813dd9233a125d482803d033
Tiziano Cialfi <tiziano.cialfi@soft.it>
2023-02-22 16:55:48 +0100

    Add docstrings and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Tiziano Cialfi on 22/02/23.
+//
+
+import Foundation
+
+/// Object that represents a single API call
+final class RMRequest {
+    
+}
