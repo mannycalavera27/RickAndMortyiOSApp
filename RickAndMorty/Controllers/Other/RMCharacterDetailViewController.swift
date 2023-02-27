9b593d03048ea6492e91a4a2dcc7fc7005012991
Tiziano Cialfi <tiziano.cialfi@soft.it>
2023-02-27 17:19:48 +0100

    Create detail view compositional layout

--- RickAndMorty/Controllers/Other/RMCharacterDetailViewController.swift
+++ RickAndMorty/Controllers/Other/RMCharacterDetailViewController.swift
@@ -11,8 +11,13 @@ import UIKit
 final class RMCharacterDetailViewController: UIViewController {
     private let viewModel: RMCharacterDetailViewViewModel
     
+    private let detailView: RMCharacterDetailView
+    
+    // MARK: - Init
+    
     init(viewModel: RMCharacterDetailViewViewModel) {
         self.viewModel = viewModel
+        self.detailView = RMCharacterDetailView(frame: .zero, viewModel: viewModel)
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -20,9 +25,58 @@ final class RMCharacterDetailViewController: UIViewController {
         fatalError("Unsupported")
     }
     
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = viewModel.title
+        view.addSubview(detailView)
+        navigationItem.rightBarButtonItem = .init(
+            barButtonSystemItem: .action,
+            target: self,
+            action: #selector(didTapShare)
+        )
+        addConstraints()
+        detailView.collectionView?.delegate = self
+        detailView.collectionView?.dataSource = self
+    }
+    
+    @objc
+    private func didTapShare() {
+        
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            detailView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            detailView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            detailView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            detailView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - CollectionView
+
+extension RMCharacterDetailViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        viewModel.sections.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        if indexPath.section == 0 {
+            cell.backgroundColor = .systemPink
+        } else if indexPath.section == 1 {
+            cell.backgroundColor = .systemGreen
+        } else {
+            cell.backgroundColor = .systemBlue
+        }
+        return cell
     }
 }
