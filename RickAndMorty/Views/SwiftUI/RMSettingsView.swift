9e98b84201becad88e6d55bcdcf6f6116b0fc13e
Tiziano Cialfi <tiziano.cialfi@soft.it>
2023-03-02 09:48:17 +0100

    SwiftUI Settings View

---
+++ RickAndMorty/Views/SwiftUI/RMSettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  RMSettingsView.swift
+//  RickAndMorty
+//
+//  Created by Tiziano Cialfi on 02/03/23.
+//
+
+import SwiftUI
+
+struct RMSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RMSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        RMSettingsView()
+    }
+}
