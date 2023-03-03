5beb6310144ffe77f818b675c869edc16d46514c
Tiziano Cialfi <tiziano.cialfi@soft.it>
2023-03-03 18:28:53 +0100

    Add no results search view

---
+++ RickAndMorty/ViewModels/Search/RMNoSearchResultsViewViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RMNoSearchResultsViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Tiziano Cialfi on 03/03/23.
+//
+
+import UIKit
+
+struct RMNoSearchResultsViewViewModel {
+    let title = "No Results"
+    let image = UIImage(systemName: "magnifyingglass.circle")
+}
